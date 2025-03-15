a10fb059c87072aee4a60b60336ab8be5489c12d
Runicu <69684251+Runicu@users.noreply.github.com>
2025-03-15 14:43:46 +0500

    Update views.py

--- bookstore_app/views.py
+++ bookstore_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib import messages
 from django.core.paginator import Paginator
 from .models import Book, CustomUser, Order, OrderItem
 from .forms import BookForm, CustomUserCreationForm, CustomUserChangeForm
